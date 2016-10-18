b93070c8fb10a7a1bf51fc853efaa2617a3935f8
Alaina Traxler and Satchel Grant <alaina+satchel@>
2016-10-17 17:50:00 -0700

    add orbit and translation fxns

--- scripts.js
+++ scripts.js
@@ -1,10 +1,38 @@
 $(document).ready(function() {
+  var away = true;
+
+  // Translation
   setInterval(function() {
-    var stringInput = $("a-sphere").attr("position");
-    console.log(stringInput.z);
-    stringInput.z -= .1;
-    console.log(stringInput.z);
-    var stringOutput = stringInput.x + " " + stringInput.y + " " + stringInput.z;
-    $("a-sphere").attr("position",  stringOutput);
+    var spherePosition = $("a-sphere").attr("position");
+
+    if(spherePosition.z >= -50 && away)
+      spherePosition.z -= .5;
+    else if (spherePosition.z <= -10 && away == false){
+      spherePosition.z += .5;
+    }
+    if(spherePosition.z <= -50)
+      away = false;
+    if(spherePosition.z >= -30)
+      away = true;
+    var newSpherePosition = spherePosition.x + " " + spherePosition.y + " " + spherePosition.z;
+    $("a-sphere").attr("position",  newSpherePosition);
   }, 100);
+
+
+  // Orbit
+  setInterval(function() {
+    var spherePosition = $("a-sphere").attr("position");
+    var sphereVector = [spherePosition.x,spherePosition.y,spherePosition.z];
+    sphereVector = orbitalGenerator(sphereVector, (Math.PI/150.0));
+    var newSpherePosition = sphereVector[0] + " " + sphereVector[1] + " " + sphereVector[2];
+    $("a-sphere").attr("position",  newSpherePosition);
+  }, 100);
+
+
 });
+
+function orbitalGenerator(vector, angle) {
+  var x = Math.cos(angle)*vector[0] - Math.sin(angle)*vector[2];
+  var z = Math.sin(angle)*vector[0] + Math.cos(angle)*vector[2];
+  return [x,vector[1],z];
+}
