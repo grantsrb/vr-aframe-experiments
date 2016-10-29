b32389d651f935c834af74e690897c6545716034
Satchel Grant <grantsrb@gmail.com>
2016-10-29 13:07:52 -0700

    add universal orbit fxn

--- scripts.js
+++ scripts.js
@@ -3,26 +3,29 @@ $(document).ready(function() {
 
   // // Translation
   // setInterval(function() {
-  //   var spherePosition = $("a-sphere").attr("position");
+  //   var horizontalSpherePosition = $("#horizontal-orbit").attr("position");
   //
-  //   if(spherePosition.z >= -50 && away)
-  //     spherePosition.z -= .5;
-  //   else if (spherePosition.z <= -10 && away == false){
-  //     spherePosition.z += .5;
+  //   if(horizontalSpherePosition.z >= -50 && away)
+  //     horizontalSpherePosition.z -= .5;
+  //   else if (horizontalSpherePosition.z <= -10 && away == false){
+  //     horizontalSpherePosition.z += .5;
   //   }
-  //   if(spherePosition.z <= -50)
+  //   if(horizontalSpherePosition.z <= -50)
   //     away = false;
-  //   if(spherePosition.z >= -30)
+  //   if(horizontalSpherePosition.z >= -30)
   //     away = true;
-  //   var newSpherePosition = spherePosition.x + " " + spherePosition.y + " " + spherePosition.z;
-  //   $("a-sphere").attr("position",  newSpherePosition);
+  //   var newSpherePosition = horizontalSpherePosition.x + " " + horizontalSpherePosition.y + " " + horizontalSpherePosition.z;
+  //   $("#horizontal-orbit").attr("position",  newSpherePosition);
   // }, 100);
 
 
   // Orbit
   setInterval(function() {
-    var spherePosition = $("a-sphere").attr("position");
-    $("a-sphere").attr("position",  orbit(spherePosition));
+    var orbitAngle = Math.PI/150.0;
+    var horizontalSpherePosition = $("#horizontal-orbit").attr("position");
+    var verticalSpherePosition = $("#vertical-orbit").attr("position");
+    $("#horizontal-orbit").attr("position",  orbit(horizontalSpherePosition, orbitAngle, 1));
+    $("#vertical-orbit").attr("position",  orbit(verticalSpherePosition, orbitAngle, 0));
 
     var boxRotation = $("a-box").attr("rotation");
     $("a-box").attr("rotation", rotate(boxRotation));
@@ -31,9 +34,9 @@ $(document).ready(function() {
 
 });
 
-function orbit(position) {
+function orbit(position, angle, rotationAxis) {
   var positionVector = [position.x,position.y,position.z];
-  positionVector = orbitalGenerator(positionVector, (Math.PI/150.0));
+  positionVector = orbitalGenerator(positionVector, angle, rotationAxis);
   return positionVector.join(" ");
 }
 
@@ -50,8 +53,24 @@ function rotationTransform(angleVector, xAng, yAng, zAng) {
   return angleVector;
 }
 
-function orbitalGenerator(vector, angle) {
-  var x = Math.cos(angle)*vector[0] - Math.sin(angle)*vector[2];
-  var z = Math.sin(angle)*vector[0] + Math.cos(angle)*vector[2];
-  return [x,vector[1],z];
+function orbitalGenerator(vector, angle, rotationAxis) {
+  var axes = findAxes(rotationAxis);
+
+  var newCoord1 = Math.cos(angle)*vector[axes[0]] - Math.sin(angle)*vector[axes[1]];
+  var newCoord2 = Math.sin(angle)*vector[axes[0]] + Math.cos(angle)*vector[axes[1]];
+
+  vector[axes[0]] = newCoord1;
+  vector[axes[1]] = newCoord2;
+
+  return vector;
+}
+
+function findAxes(rotationAxis) {
+  var axes = [];
+  for(var i = 0; i < 3; i++) {
+    if(i != rotationAxis) {
+      axes.push(i);
+    }
+  }
+  return axes;
 }
